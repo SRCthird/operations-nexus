a68618553516b3fef04d1cd4238aaccf1a08b05a
SRCthird <SRCthird@gmail.com>
2023-10-03 23:38:08 -0400

    Add 404 page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import '../styles/NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className="NotFound">
+            <h1 className="NotFound-Title">404</h1>
+            <p>Oops! Something is wrong.</p>
+            <a className="button" href="/"><i className="icon-home"></i> Go back in initial page, is better.</a>
+        </div>
+    );
+};
+
+export default NotFound;
