bc1900f3b79e78e5ebeed98879becc33f1b5eced
SRCthird <SRCthird@gmail.com>
2023-10-04 20:56:59 -0400

    Create secondary slides webhook

---
+++ src/webhooks/useSecondarySlides.ts
@@ -0,0 +1,51 @@
+import axios, { CanceledError } from "axios";
+import { useEffect, useState } from "react";
+import Slides from './useMainSlides';
+
+/**
+ * Properties of the Secondary Slides webhook.
+ * 
+ * @param {string} location - The name of the directory of slide in the backend.
+ */
+interface Props {
+    location?: string
+}
+
+/**
+ * The type returned from the webhook.
+ * 
+ * @param {Slides[]} secondarySlides - The list of slides in the secondary folder.
+ * @param {string} slideError - The error message if the secondary folder cannot be retrieved.
+ */
+type useSecondarySlides = {
+    secondarySlides: Slides[];
+    secondarySlideError: string;
+}
+
+/**
+ * This webhook is used to retieve the list of secondary slides from the backend.
+ * 
+ * @param {interface} Props - The properties of the Secondary Slides webhook.
+ * @returns {useSecondarySlides} - The object returned from the webhook.
+ */
+const useSecondarySlides = ({location}:Props): useSecondarySlides => {
+    const [secondarySlides, setSecondarySlides] = useState<Slides[]>([]);
+    const [secondarySlideError, setError] = useState('');
+
+    useEffect(() => {
+      const controller = new AbortController();
+      axios.get(`/api/${location}`, { signal: controller.signal })
+        .then(response => {
+            setSecondarySlides(response.data);
+        })
+        .catch(err => {
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+      });
+      return () => controller.abort();
+    }, [location]);
+
+    return { secondarySlides, secondarySlideError }
+}
+
+export default useSecondarySlides;
