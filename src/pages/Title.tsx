d79b90bec2f0265354cc9fd7745ce0881334d2bc
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-12 19:20:38 -0500

    Create Title element

---
+++ src/pages/Title.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import { Grid, GridItem, Text } from '@chakra-ui/react';
+
+interface Props {
+    title: string;
+}
+
+const Title = ({ title }: Props): JSX.Element => {
+    return (
+        <Grid templateColumns="repeat(3, 1fr)" gap={6} marginBottom={6} >
+            <GridItem className="Empty" w="100%" h="10vh" />
+            <GridItem className="Title" w="100%" h="10vh">
+                <Text className='text-center my-3 title' fontSize={44}>{title}</Text>
+            </GridItem>
+            <GridItem className="Empty" w="100%" h="10vh" />
+        </Grid>
+    )
+}
+
+export default Title
