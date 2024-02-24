aeefefab53c3bbba13c78be7786999c49badf56b
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-23 20:34:12 -0500

    Init departments REST API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { DisplaysModule } from './displays/displays.module';
+import { DepartmentsModule } from './departments/departments.module';
 
 @Module({
-  imports: [DatabaseModule, DisplaysModule],
+  imports: [DatabaseModule, DisplaysModule, DepartmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
