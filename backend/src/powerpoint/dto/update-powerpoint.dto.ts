79eda8203392b357f4c5f403a555c3cef3451044
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-23 21:18:40 -0500

    Remove unused dto's and entities

--- backend/src/powerpoint/dto/update-powerpoint.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePowerpointDto } from './create-powerpoint.dto';
-
-export class UpdatePowerpointDto extends PartialType(CreatePowerpointDto) {}
