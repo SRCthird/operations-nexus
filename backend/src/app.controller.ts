b8d6ba43c57247161ed505030213943809485487
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-23 22:51:07 -0500

    Create delay endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Patch } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('delay')
+  async getDelay(): Promise<number> {
+    return this.appService.getDelay();
+  }
+
+  @Get('delay/:delay')
+  async setDelay(@Param('delay') delay: string): Promise<string> {
+    return this.appService.setDelay(+delay);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class AppService {
+  
+  constructor(private readonly databaseService: DatabaseService) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getDelay(): Promise<number> {
+    let result = await this.databaseService.settings.findFirst({
+      select: {
+        Delay: true,
+      }
+    });
+    return result ? result.Delay : 30000;
+  }
+
+  async setDelay(delay: number): Promise<string> {
+    await this.databaseService.settings.update({
+      where: {ID: 1},
+      data: {Delay: delay}
+    })
+    return `Delay set to ${delay}.`
   }
 }
