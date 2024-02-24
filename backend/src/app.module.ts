6bcfb6d5b8af033870ec6abd6818b969f9cc4d0b
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-23 20:38:01 -0500

    Init static image API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { DisplaysModule } from './displays/displays.module';
 import { DepartmentsModule } from './departments/departments.module';
 import { PowerpointModule } from './powerpoint/powerpoint.module';
+import { StaticModule } from './static/static.module';
 
 @Module({
-  imports: [DatabaseModule, DisplaysModule, DepartmentsModule, PowerpointModule],
+  imports: [DatabaseModule, DisplaysModule, DepartmentsModule, PowerpointModule, StaticModule],
   controllers: [AppController],
   providers: [AppService],
 })
