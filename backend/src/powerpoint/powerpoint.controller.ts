cc963e2f36be5496e5625efbeb0bf45e995d7268
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-23 20:39:33 -0500

    Setup powerpoint api

--- backend/src/powerpoint/powerpoint.controller.ts
+++ backend/src/powerpoint/powerpoint.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Patch, 
+         Param, Delete, UseInterceptors, 
+         UploadedFile, ParseFilePipe, 
+         MaxFileSizeValidator, FileTypeValidator, 
+         Res } from '@nestjs/common';
 import { PowerpointService } from './powerpoint.service';
-import { CreatePowerpointDto } from './dto/create-powerpoint.dto';
-import { UpdatePowerpointDto } from './dto/update-powerpoint.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 
 @Controller('powerpoint')
 export class PowerpointController {
   constructor(private readonly powerpointService: PowerpointService) {}
 
-  @Post()
-  create(@Body() createPowerpointDto: CreatePowerpointDto) {
-    return this.powerpointService.create(createPowerpointDto);
+  @Post(':location')
+  @UseInterceptors(FileInterceptor('file'))
+  async create(@Param('location') location: string, @UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 1_073_741_824 }), // Allow up to 1 GB
+        new FileTypeValidator({ fileType: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' }),
+      ],
+    }),
+  ) file: Express.Multer.File) {
+    return this.powerpointService.create(location, file);
   }
 
-  @Get()
-  findAll() {
-    return this.powerpointService.findAll();
+  @Get(':location')
+  async findAll(@Param('location') location?: string) {
+    return this.powerpointService.findAll(location);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.powerpointService.findOne(+id);
+  @Get(':location/:name')
+  async findOne(@Param('location') location: string, @Param('name') encodedName: string, @Res() res: Response) {
+    const name = decodeURIComponent(encodedName);
+    const filePath = await this.powerpointService.findOne(location, name);
+    res.setHeader('Content-Disposition', `attachment; filename="${name}"`);
+    res.sendFile(filePath);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePowerpointDto: UpdatePowerpointDto) {
-    return this.powerpointService.update(+id, updatePowerpointDto);
+  @Patch(':location/:name')
+  @UseInterceptors(FileInterceptor('file'))
+  async update(@Param('location') location: string, @Param('name') encodedName: string, @UploadedFile(
+    new ParseFilePipe({
+      validators: [
+        new MaxFileSizeValidator({ maxSize: 1_073_741_824 }), // Allow up to 1 GB
+        new FileTypeValidator({ fileType: 'application/vnd.openxmlformats-officedocument.presentationml.presentation' }),
+      ],
+    }),
+  ) file: Express.Multer.File) {
+    const name = decodeURIComponent(encodedName);
+    return this.powerpointService.update(location, name, file);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.powerpointService.remove(+id);
+  @Delete(':location/:name')
+  async remove(@Param('location') location: string, @Param('name') name: string) {
+    return this.powerpointService.remove(location, name);
   }
 }
--- backend/src/powerpoint/powerpoint.service.ts
+++ backend/src/powerpoint/powerpoint.service.ts
@@ -1,26 +1,135 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePowerpointDto } from './dto/create-powerpoint.dto';
-import { UpdatePowerpointDto } from './dto/update-powerpoint.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class PowerpointService {
-  create(createPowerpointDto: CreatePowerpointDto) {
-    return 'This action adds a new powerpoint';
+  constructor(private readonly databaseService: DatabaseService) { }
+  private readonly STATIC_DIR = path.join(__dirname, '..', 'public');
+
+  /**
+   *  CRUD definitions 
+   */
+
+  async create(location: string, file: Express.Multer.File): Promise<any> {
+    if (!this.validateLocation(location)) {
+      throw new NotFoundException(`location '${location}' does not exist.`);
+    }
+
+    const directoryPath = path.join(this.STATIC_DIR, 'pptx', location);
+    fs.mkdirSync(directoryPath, { recursive: true });
+    const filePath = path.join(directoryPath, file.originalname);
+    fs.writeFileSync(filePath, file.buffer);
+
+    return {
+      message: 'File uploaded successfully',
+      filePath: filePath,
+    };
   }
 
-  findAll() {
-    return `This action returns all powerpoint`;
+  async findAll(location?: string): Promise<string[]> {
+    if (!(await this.validateLocation(location))) {
+      throw new NotFoundException(`location '${location}' does not exist.`);
+    }
+    
+    let response = []
+    const dir = path.join(this.STATIC_DIR, 'pptx', location);
+
+    if (!fs.existsSync(dir)) {
+      fs.mkdir(dir, { recursive: true }, (mkdirErr) => {
+        if (mkdirErr) {
+          throw new NotFoundException(`Failed to create directory '${location}'`);
+        }
+      });
+    }
+    response = this.readDirectory(location, '.pptx');
+    return response
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} powerpoint`;
+  async findOne(location: string, name: string): Promise<string> {
+    const filePath = path.join(this.STATIC_DIR, 'pptx', location, name);
+    
+    console.log(filePath);
+
+    try {
+      await fs.promises.access(filePath, fs.constants.F_OK);
+      return filePath;
+    } catch (err) {
+      throw new NotFoundException(`File '${name}' not found in location '${location}'.`);
+    }
   }
 
-  update(id: number, updatePowerpointDto: UpdatePowerpointDto) {
-    return `This action updates a #${id} powerpoint`;
+  async update(location: string, name: string, file: Express.Multer.File): Promise<object> {
+    if (!(await this.validateLocation(location))) {
+      throw new NotFoundException(`Location '${location}' does not exist.`);
+    }
+
+    const directoryPath = path.join(this.STATIC_DIR, 'pptx', location);
+    let filePath: string = path.join(directoryPath, name);
+
+    if (!fs.existsSync(filePath)) {
+      throw new NotFoundException(`File '${name}' not found in location '${location}'.`);
+    }
+
+    fs.writeFileSync(filePath, file.buffer);
+    
+    if (name != file.originalname){
+      const newFilePath: string = path.join(directoryPath, file.originalname);
+      if (fs.existsSync(newFilePath)) {
+        throw new Error(`File '${file.originalname}' already exists in location '${location}'.`);
+      }
+      fs.renameSync(filePath, newFilePath);
+      filePath = newFilePath
+    }
+
+    return {
+      message: 'File updated successfully',
+      filePath: filePath,
+    };
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} powerpoint`;
+  async remove(location: string, name: string): Promise<object> {
+    if (!(await this.validateLocation(location))) {
+      throw new NotFoundException(`Location '${location}' does not exist.`);
+    }
+
+    const filePath = path.join(this.STATIC_DIR, 'pptx', location, name);
+
+    if (!fs.existsSync(filePath)) {
+      throw new NotFoundException(`File '${name}' not found in location '${location}'.`);
+    }
+
+    fs.unlinkSync(filePath);
+
+    return {
+      message: 'File removed successfully',
+      filePath: filePath,
+    };
+  }
+
+
+  /**
+   *  Utility Methods
+   */
+
+  async validateLocation(location: string): Promise<boolean> {
+    const result = await this.databaseService.departments.findUnique({
+      where: { Department: location },
+    });
+    return !!result;
+  }
+  
+  readDirectory(location: string, extension: string): string[] {
+    const dir = path.join(this.STATIC_DIR, 'pptx', location);
+    try {
+      const files = fs.readdirSync(dir);
+      const allFiles = files
+        .filter(file => file.toLowerCase().endsWith(extension))
+        .map(file => path.join(location, file));
+      return allFiles;
+    } catch (err) {
+      throw new NotFoundException(`Failed to read '${location}'`);
+    }
   }
 }
