189c29a27e7c6e28a8950534fdd36b72fa3140cd
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-23 21:52:34 -0500

    Add settings to database

---
+++ backend/prisma/migrations/20240224025202_init_settings/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `settings` (
+    `ID` INTEGER NOT NULL AUTO_INCREMENT,
+    `Display` INTEGER NOT NULL DEFAULT 30000,
+
+    PRIMARY KEY (`ID`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
