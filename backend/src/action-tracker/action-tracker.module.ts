f960b285b471585737611accc2e1baf5953f37ba
Stephen Chryn <M312559@one.merckgroup.com>
2024-02-29 11:45:07 -0500

    Update dependency

--- backend/src/action-tracker/action-tracker.module.ts
+++ backend/src/action-tracker/action-tracker.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ActionTrackerService } from './action-tracker.service';
 import { ActionTrackerController } from './action-tracker.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ActionTrackerController],
   providers: [ActionTrackerService],
 })
