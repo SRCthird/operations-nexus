a39f0d1c9599fdedd29d8208a02fe18f2e028e2b
Stephen Chryn <M312559@one.merckgroup.com>
2024-03-04 12:25:02 -0500

    Create a frontend production script

---
+++ serve-script.js
@@ -0,0 +1,20 @@
+// serve.js
+require('dotenv').config();
+const { exec } = require('child_process');
+
+// Construct the serve command with SSL options if needed
+let serveCommand = 'serve -s build';
+if (process.env.HTTPS === 'true') {
+  serveCommand += ` --ssl-cert ${process.env.SSL_CERT_PATH} --ssl-key ${process.env.SSL_KEY_PATH}`;
+}
+
+// Execute the serve command
+exec(serveCommand, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+});
+
