e8809c366327f96386de59241378e9bc53d6ea3a
Stephen Chryn <SRCthird@gmail.com>
2024-03-12 10:46:03 -0400

    Create selection menu Mobile view

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,9 +1,10 @@
-import { Box, Grid, Heading, Button, } from "@chakra-ui/react"
+import { Box, Grid, Heading, Button, MenuButton, Menu, MenuList, MenuItem, } from "@chakra-ui/react"
 import { useState } from "react";
 import "../styles/Admin.css"
 import AdminDisplays from "../components/AdminDisplays";
 import AdminDepartments from "../components/AdminDepartments";
 import AdminPowerPoints from "../components/AdminPowerPoints";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 /**
  * Properties for the Admin dashboard.
@@ -25,11 +26,29 @@ const Admin = ({ title }: Props): JSX.Element => {
 
   return (
     <Box className="Admin-Container">
-      <Grid className="Admin-Title" templateColumns={'1fr 200px 200px 200px'} gap={'15px'} padding={'15px'}>
+      <Grid className="Admin-Title" templateColumns={'1fr 200px'} gap={'15px'} padding={'15px'}>
         <Heading as="h1" h="max-content">{title}</Heading>
-        <Button alignSelf={'center'} onClick={() => { setDisplay(0) }}> Departments</Button>
-        <Button alignSelf={'center'} onClick={() => { setDisplay(1) }}> Displays</Button>
-        <Button alignSelf={'center'} onClick={() => { setDisplay(2) }}> SlideShows</Button>
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton 
+                isActive={isOpen} 
+                as={Button} 
+                rightIcon={<ChevronDownIcon />}
+              >
+                {isOpen? 
+                  'Close views': 
+                  display===0? 'Departments':
+                  display===1? 'Displays': 'SlideShows'
+                }
+              </MenuButton>
+              <MenuList>
+                <MenuItem onClick={() => { setDisplay(0) }}>Departments</MenuItem>
+                <MenuItem onClick={() => { setDisplay(1) }}>Displays</MenuItem>
+                <MenuItem onClick={() => { setDisplay(2) }}>SlideShows</MenuItem>
+              </MenuList>
+            </>)}
+        </Menu>
       </Grid>
       {display === 0 && <AdminDepartments />}
       {display === 1 && <AdminDisplays />}
