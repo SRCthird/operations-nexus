c6de12bbaeaa79257e991ad8b3c23e0835aa9f3f
Stephen Chryn <SRCthird@gmail.com>
2024-03-14 16:20:01 -0400

    Create the page template webhook to retrieve templates from the database

---
+++ src/webhooks/usePages.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosError } from "axios";
+import { useEffect, useState } from "react";
+
+/**
+ * The pages available for selection in the admin menu.
+ */
+export enum Pages {
+  ThreeOnTwo = 'ThreeOnTwo',
+  FullDisplay = 'FullDisplay'
+}
+
+const usePages = (page?: Pages, ids?: number[]) => {
+  const [pages, setPages] = useState<any[]>([]);
+  const [pageError, setError] = useState('');
+  const [isPageLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setLoading(true);
+    const idArray = ids ? ids : [];
+    axios.get(`/api/page/${page}/?id=${idArray.join(",")}`, { signal: controller.signal })
+      .then(response => {
+        if (JSON.stringify(pages) !== JSON.stringify(response.data)) {
+          setPages(response.data);
+        }
+        setLoading(false);
+      })
+      .catch((err: AxiosError) => {
+        if (err.response && err.response.status === 404) {
+          setError(err.message);
+        } else {
+          setError(err.message);
+        }
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, [ids, page, pages]);
+
+  return { pages, pageError, isPageLoading };
+}
+
+export default usePages;
