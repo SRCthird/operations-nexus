7cace5602d1e83471f134868805b9e3a52af36eb
Stephen Chryn <SRCthird@gmail.com>
2024-03-20 13:13:25 -0400

    Remove exclusion of static from the /api prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,13 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api', {
-    exclude: [
-      'static',
-      'static/:location',
-      'static/:location/:name'
-    ]
-  });
+  app.setGlobalPrefix('api');
   await app.listen(5000);
 }
 bootstrap();
