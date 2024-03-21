ed6524a2f04358ba6ac8a04be4d24a975e34dbca
Stephen Chryn <SRCthird@gmail.com>
2024-03-21 13:41:10 -0400

    Click the title takes you home now

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,15 +1,23 @@
 import { Grid, GridItem, Text } from '@chakra-ui/react';
+import { useNavigate  } from "react-router-dom";
 
 interface Props {
     title: string;
 }
 
 const Title = ({ title }: Props): JSX.Element => {
+  const navigate = useNavigate();
+  
+  const handleClick = () => {
+    navigate('/');
+  }
     return (
         <Grid templateColumns="repeat(3, 1fr)" gap={6} marginBottom={6} >
             <GridItem className="Empty" w="100%" h="10vh" />
             <GridItem className="Title" w="100%" h="10vh">
-                <Text className='text-center my-3 title' fontSize={44}>{title}</Text>
+                <Text onClick={handleClick} className='text-center my-3 title' fontSize={44}>
+                  {title}
+                </Text>
             </GridItem>
             <GridItem className="Empty" w="100%" h="10vh" />
         </Grid>
