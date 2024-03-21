b985f5e7caebb502479bacfb516e159a0e651535
Stephen Chryn <SRCthird@gmail.com>
2024-03-21 13:39:26 -0400

    Update NotFound to redirect the "React Way"

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,21 @@
+import { Button } from '@chakra-ui/react';
 import '@styles/NotFound.css';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/');
+  }
     return (
         <div className="NotFound">
             <h1 className="NotFound-Title">404</h1>
             <p>Oops! Something is wrong.</p>
-            <a className="button" href="/"><i className="icon-home"></i> Go back in initial page, is better.</a>
+            <Button className="button" onClick={handleClick}>
+              <i className="icon-home"></i> 
+              Go back home, is better there.
+            </Button>
         </div>
     );
 };
