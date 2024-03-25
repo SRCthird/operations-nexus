f764ea9c4c186ea81dd16cd06c4c98b92db3f58f
Stephen Chryn <SRCthird@gmail.com>
2024-03-25 12:56:33 -0400

    App port to production builder

--- serve-script.js
+++ serve-script.js
@@ -4,6 +4,7 @@ const { exec } = require('child_process');
 
 // Construct the serve command with SSL options if needed
 let serveCommand = 'serve -s build';
+serveCommand += ` --listen ${process.env.PORT || "8080"}` 
 if (process.env.HTTPS === 'true') {
   serveCommand += ` --ssl-cert ${process.env.SSL_CERT_PATH} --ssl-key ${process.env.SSL_KEY_PATH}`;
 }
