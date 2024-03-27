873ce42efc72f72483b07a2a4c99883549753841
Stephen Chryn <SRCthird@gmail.com>
2024-03-27 14:32:29 -0400

    Update Aliases

--- craco.config.js
+++ craco.config.js
@@ -6,10 +6,11 @@ module.exports = {
       '@src': path.resolve(__dirname, 'src/'),
       '@styles': path.resolve(__dirname, 'src/styles/'),
       '@pages': path.resolve(__dirname, 'src/pages/'),
-      '@templates': path.resolve(__dirname, 'src/templates/'),
+      '@templates': path.resolve(__dirname, 'src/plugins/templates/'),
       '@hooks': path.resolve(__dirname, 'src/webhooks/'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@context': path.resolve(__dirname, 'src/context/'),
+      '@apps': path.resolve(__dirname, 'src/plugins/apps/'),
     }
   },
 };
