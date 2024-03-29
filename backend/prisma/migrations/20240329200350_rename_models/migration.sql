9c5822dcf2b4bc69b0c4bf81fe392daed4cf6134
Stephen Chryn <SRCthird@gmail.com>
2024-03-29 17:12:02 -0400

    Rename tables to fit typescript types

---
+++ backend/prisma/migrations/20240329200350_rename_models/migration.sql
@@ -0,0 +1,33 @@
+-- AlterTable
+ALTER TABLE `nexus_display` RENAME COLUMN `Page` TO `Template`;
+
+-- AlterTable 
+ALTER TABLE `page_fulldisplay` RENAME TO `Template_FullDisplay`;
+
+-- AlterTable 
+ALTER TABLE `page_fulldisplay2` RENAME TO `Template_FullDisplay2`;
+
+-- AlterTable 
+ALTER TABLE `page_fulldisplay3` RENAME TO `Template_FullDisplay3`;
+
+-- AlterTable 
+ALTER TABLE `page_fulldisplay4` RENAME TO `Template_FullDisplay4`;
+
+-- AlterTable 
+ALTER TABLE `page_fulldisplay5` RENAME TO `Template_FullDisplay5`;
+
+-- AlterTable 
+ALTER TABLE `page_fullwithcircle` RENAME TO `Template_FullWithCircle`;
+
+-- AlterTable 
+ALTER TABLE `page_onebythree` RENAME TO `Template_OneByThree`;
+
+-- AlterTable 
+ALTER TABLE `page_splitscreen` RENAME TO `Template_SplitScreen`;
+
+-- AlterTable 
+ALTER TABLE `page_threeontwo` RENAME TO `Template_ThreeOnTwo`;
+
+-- AlterTable 
+ALTER TABLE `page_twobytwo` RENAME TO `Template_TwoByTwo`;
+
