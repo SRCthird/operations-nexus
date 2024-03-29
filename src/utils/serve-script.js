13b79e292b198c5cfb445e2b21c81f415f39a01a
Stephen Chryn <SRCthird@gmail.com>
2024-03-29 14:47:42 -0400

    Move serve to src/utils

--- src/utils/serve-script.js
+++ src/utils/serve-script.js
@@ -1,15 +1,12 @@
-// serve.js
 require('dotenv').config();
 const { exec } = require('child_process');
 
-// Construct the serve command with SSL options if needed
 let serveCommand = 'serve -s build';
 serveCommand += ` --listen ${process.env.PORT || "8080"}` 
 if (process.env.HTTPS === 'true') {
   serveCommand += ` --ssl-cert ${process.env.SSL_CERT_PATH} --ssl-key ${process.env.SSL_KEY_PATH}`;
 }
 
-// Execute the serve command
 exec(serveCommand, (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
