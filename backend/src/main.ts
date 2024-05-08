5d88c9b4d0ca9b36f93ac8b589e3d4699e3793e0
Stephen Chryn <SRCthird@gmail.com>
2024-05-08 15:52:59 -0400

    set abortOnError to false and add SSL

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { CoreModule } from './core.module';
+import { NestApplicationOptions } from '@nestjs/common';
+import * as fs from 'fs';
+import 'dotenv/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(CoreModule);
+  const options: NestApplicationOptions = { abortOnError: false };
+  if (process.env.HTTPS === 'true') {
+    options.httpsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY_PATH),
+      cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+    };
+  }
+
+  const app = await NestFactory.create(CoreModule, { ...options });
   app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(5000);
